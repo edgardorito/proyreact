0db101c67a8bc3e43bcee1ccfd8f6ca21890510c
edgardorito <edgardorito@gmail.com>
2018-02-24 13:43:13 -0600

    Probando react

--- src/App.js
+++ src/App.js
@@ -2,18 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Title from './components/Title';
+import { hola } from './components/Title';
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      numero: 0
+    };
+
+    this.updateNumero = this.updateNumero.bind(this);
+
+  }
+
+  updateNumero(){
+    this.setState({
+        numero: this.state.numero + 1
+      });
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <section>
+        <div>
+          <div>
+            <Title></Title>
+            <h2>{this.state.numero}</h2>
+            <button onClick={this.updateNumero}>Crear cuenta gratuita</button>
+            <img src={process.env.PUBLIC_URL + '/images/amigo.jpg'} height="200px"/>
+            <div>
+              <ul>
+                <li>
+                  <h3>Calificacion con emociones</h3>
+                  <p>Califica tus lugares con experencias, no con numeros</p>
+                </li>
+                <li>
+                  <h3>¿Sin internet? Sin problemas</h3>
+                  <p>Esta pagina funciona sin internet o conexiones lentas</p>
+                </li>
+                <li>
+                  <h3>Tus lugares favoritos</h3>
+                  <p>Define tu lista de sitios favoritos</p>
+                </li>
+              </ul>
+            </div>
+        </div>
+        </div>
+      </section>
     );
   }
 }
---
+++ src/components/Title.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Title extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Proyecto con React</h1>
+        <p>Descubre lugares de manera simple </p>
+      </div>
+    );
+  }
+}
+
+export function hola() {
+  return "Hola";
+}
